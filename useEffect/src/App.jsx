dd7f2203caea6711a2ee6f81de9d1d8317ef4398
Mohdfaraz21 <mofaraz7800@gmail.com>
2025-07-04 01:35:22 +0530

    basic of useEffect

---
+++ useEffect/src/App.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(1);
+
+  function increaseCount() {
+    setCount((currentValue) => currentValue + 1);
+  }
+
+  useEffect(function () {
+    console.log("above setInterval");
+    setInterval(increaseCount, 1000);
+  }, []);
+  return <div>{count}</div>;
+}
+
+export default App;
