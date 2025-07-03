e7a783cba867fb87c54483a7789e5ae35acac740
Mohdfaraz21 <mofaraz7800@gmail.com>
2025-07-04 02:05:38 +0530

    more on effect

--- useEffect/src/App.jsx
+++ useEffect/src/App.jsx
@@ -2,16 +2,29 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [count, setCount] = useState(1);
+  const [count2, setCount2] = useState(1);
 
   function increaseCount() {
     setCount((currentValue) => currentValue + 1);
   }
+  function decreaseCount() {
+    setCount2((currentValue) => currentValue - 1);
+  }
 
+  // this effect run only once, when it mount to dom
   useEffect(function () {
     console.log("above setInterval");
     setInterval(increaseCount, 1000);
-  }, []);
-  return <div>{count}</div>;
+    setInterval(decreaseCount, 5000);
+  }, []); // this effect will run on mount, because the array is empty.
+
+
+  // this side effects run anytime count changes.
+  useEffect(function() {
+    console.log("the count has been updated to " + count);
+  }, [count, count2])
+
+  return <div>{count} {count2}</div>;
 }
 
 export default App;
