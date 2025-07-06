a627e3446f6063baade2ebfc1cc0ddf9093f4f97
Mohdfaraz21 <mofaraz7800@gmail.com>
2025-07-06 21:36:19 +0530

    add few line of comment

--- useRef/DOMwithRef/src/App.jsx
+++ useRef/DOMwithRef/src/App.jsx
@@ -2,14 +2,19 @@ import { useRef } from "react";
 import "./App.css";
 
 function App() {
-  const inputRef = useRef();
+  //useRef is a Hook to create a persistent reference to a DOM node
+  const inputRef = useRef(); 
 
   function focusOnInput() {
+    //inputRef.current that access the actual elt after render.
+    //.focus() is Native DOM method to focus the input.
     inputRef.current.focus();
   }
 
   return (
+    //ref={inputRef} Bind the input elt to that reference.
     <div>
+      
       SignUp
       <input ref={inputRef} id="name" type={"text"}></input>
       <input type={"text"}></input>
