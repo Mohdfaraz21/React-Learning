42e436e15897f2d01341d6cb3f49ae53d0294b42
Mohdfaraz21 <mofaraz7800@gmail.com>
2025-07-09 00:53:13 +0530

    example of lifting state

--- RollingUpState/src/App.jsx
+++ RollingUpState/src/App.jsx
@@ -1,14 +1,45 @@
-
+import { useState } from 'react'
 import './App.css'
 
 function App() {
-  
+  return <div>
+    <LightBulb />
+  </div>
+}
 
-  return (
-   <div>
-    hiii
-   </div>
-  )
+function LightBulb() {
+  const [bulbOn, setBulbOn] = useState(true)
+
+  /* bulbOn is a prop to the Bulb State component.
+  bulbOn, setBulbOn are props tot he ToggleBulbState component. */
+  return <div>
+    <BulbState bulbOn={bulbOn} />
+    <ToggleBulbState bulbOn={bulbOn} setBulbOn={setBulbOn} />
+  </div>
+}
+
+function BulbState({bulbOn}) {
+  return <div>
+    {bulbOn ? "Bulb on" : "Bulb off"}
+  </div>
+}
+
+function ToggleBulbState({bulbOn, setBulbOn}) {
+
+  function toggle() {
+    setBulbOn(!bulbOn)
+    
+  }
+
+  return <div>
+    <button onClick={toggle}>Toggle the bulb</button>
+  </div>
 }
 
 export default App
+
+/* Rolling up state means the same as "lifting state up", and its
+all about moving shared state to the closest common parent of two or more 
+components that need to access or update 
+> Instead of duplicating state in each components, you can lift the state up to the LOWEST COMMON ANCESTOR,
+allowing the common ancestor to manage it.*/
