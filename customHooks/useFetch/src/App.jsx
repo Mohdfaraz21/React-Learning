e415bf7b85c2c35d27a328fdc8982d1a08aaace4
Mohdfaraz21 <mofaraz7800@gmail.com>
2025-07-12 02:43:43 +0530

    add few comments

--- customHooks/useFetch/src/App.jsx
+++ customHooks/useFetch/src/App.jsx
@@ -3,11 +3,17 @@ import "./App.css";
 import { useFetch } from "./hooks/useFetch";
 
 function App() {
+  /* state "currentPost" is used to track which post number (1,2,3) to fetch.
+  initially set 1 */
   const [currentPost, setCurrentPost] = useState(1);
+  /* => call your custom hook useFetch() with a dynamic URL.
+  => this trigger a fetch to a different post depending on currentPost.
+  => destructure the returned object to get finalData and loading. */
   const { finalData, loading } = useFetch(
     "https://jsonplaceholder.typicode.com/posts/" + currentPost
   );
-
+  /* => while loading is true, the component shows a loading message.
+  => React will return early and not render anything else until loading false. */
   if (loading) {
     return <div>Loading.....</div>;
   }
