d55cdc4564bd53279867f2aac8f524b6704366e4
Mohdfaraz21 <mofaraz7800@gmail.com>
2025-07-14 23:42:34 +0530

    useDebounce hook

--- customHooks/useDebounce/src/App.jsx
+++ customHooks/useDebounce/src/App.jsx
@@ -1,14 +1,30 @@
 
+import { useRef } from 'react'
 import './App.css'
 
+function useDebounce(originalFn) {
+  const currentClock = useRef();
+
+  const fn = () => {
+    clearTimeout(currentClock.current);
+    currentClock.current = setTimeout(originalFn, 200);
+  }
+  return fn;
+}
+
+
 function App() {
-  
+  function sendDataToBackend() {
+    fetch("api.amazon.com/search/");
+  }
+
+  const debounceFn = useDebounce(sendDataToBackend)
 
   return (
     <>
-      
+      <input type="text" onChange={debounceFn}></input>
     </>
-  )
+  );
 }
 
 export default App
